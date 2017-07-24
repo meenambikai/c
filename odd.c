18469cceced7fc4d6ddc45769e250db24c7f50b9
meenambikai <30317603+meenambikai@users.noreply.github.com>
2017-07-24 15:17:38 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#includ<conio.h.
+int main()
+{
+int low,flag,i,flag;
+printf(" enter the number between two intervals:");
+scanf("%d %d",&low,&high);
+while(low<high)
+{
+flag=0;
+for(i=2;i<=low/2;++i)
+{
+if(low%i==0)
+{
+flag==0;
+printf("%d",low);
+++low;
+}
+return 0;
+}
