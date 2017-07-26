486f19f39b4b1732fa880e4a1d89f1e374473039
meenambikai <30317603+meenambikai@users.noreply.github.com>
2017-07-26 16:40:11 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int f=1,i,number;
+printf("enter  a number:");
+scanf("%d",&number);
+for(i=i;i<=num;i++)
+{
+f=f*i;
+}
+printf("the factorial of %d is ");
+return 0;
+}
